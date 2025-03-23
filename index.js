29915e125c6f7b30cb4a5f6d436b1239473fac96
Efoe Max <efoemaxi17@gmail.com>
2025-03-23 21:26:21 +0100

    testing the logic

--- index.js
+++ index.js
@@ -3,6 +3,3 @@ let input = document.querySelector("input");
 let otpreq = document.querySelector(".btn-otp");
 let otpval = document.querySelector(".btn-val");
 
-otpreq.addEventListener("clcik", (e)=>{
-    
-})
