69d083a7ae6dace3eb7e019f41a6b774808262cc
Efoe Max <efoemaxi17@gmail.com>
2025-03-25 19:37:17 +0100

    notifications up and running

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ let input = document.querySelector("input");
 let otpreq = document.querySelector(".btn-otp");
 let otpval = document.querySelector(".btn-val");
 
+otpreq.addEventListener("click", (e)=>{
+    Notification.requestPermission().then((perm) =>{
+        new Notification("ChatiMax Client Support!", {
+            
+        })
+    })
+})
