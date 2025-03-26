fb0663c55648d770c79e21d0933d55d37acf665f
Efoe Max <efoemaxi17@gmail.com>
2025-03-26 20:29:52 +0100

    gotten json data success

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let otpval = document.querySelector(".btn-val");
 otpreq.addEventListener("click", (e)=>{
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function(){
-        if(this.readystate == 4 & this.status == 200){
+        if(this.readyState == 4 & this.status == 200){
             xhr.responseText;
             console.log(this.responseText);
         }
