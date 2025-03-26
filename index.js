42a6c5aa60fb1829ea31e66021281ad8fd2527b2
Efoe Max <efoemaxi17@gmail.com>
2025-03-26 21:00:22 +0100

    actually working but testing logic

--- index.js
+++ index.js
@@ -7,16 +7,22 @@ otpreq.addEventListener("click", (e)=>{
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function(){
         if(this.readyState == 4 & this.status == 200){
-            xhr.responseText;
-            console.log(this.responseText);
+            let jsOb = JSON.parse(xhr.responseText);
+            let randNum = Math.trunc(Math.random() * jsOb.length) + 1;
+              let rand = jsOb[randNum].id; 
+        
+   
+  Notification.requestPermission().then((perm) =>{
+    
+        new Notification("ChatiMax Client Support!", {
+            body:`OTP CODE: ${rand}`,
+            icon:"pic.jfif"
+        })
+    })
         }
     }
+    
     xhr.open("GET", "index.json", "true");
     xhr.send();
-    // Notification.requestPermission().then((perm) =>{
-    //     new Notification("ChatiMax Client Support!", {
-    //         body:`OTP CODE: `,
-    //         icon:"pic.jfif"
-    //     })
-    // })
+  
 })
