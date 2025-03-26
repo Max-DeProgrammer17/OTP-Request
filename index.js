24ec45d0d6a30578cb332476cbb993d20c4f29b1
Efoe Max <efoemaxi17@gmail.com>
2025-03-26 20:26:17 +0100

    getting json file

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ let otpreq = document.querySelector(".btn-otp");
 let otpval = document.querySelector(".btn-val");
 
 otpreq.addEventListener("click", (e)=>{
-    Notification.requestPermission().then((perm) =>{
-        new Notification("ChatiMax Client Support!", {
-            body:"OTP CODE: ",
-            icon:"pic.jfif"
-        })
-    })
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(this.readystate == 4 & this.status == 200){
+            xhr.responseText;
+            console.log(this.responseText);
+        }
+    }
+    xhr.open("GET", "index.json", "true");
+    xhr.send();
+    // Notification.requestPermission().then((perm) =>{
+    //     new Notification("ChatiMax Client Support!", {
+    //         body:`OTP CODE: `,
+    //         icon:"pic.jfif"
+    //     })
+    // })
 })
